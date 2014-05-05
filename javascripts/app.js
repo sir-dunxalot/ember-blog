1c250df23e70b3ee424dbc983405c8301fc735f0
Duncan Graham Walker <walkerdu@bc.edu>
2014-05-04 23:14:08 -0400

    no more fouc on nav load

--- javascripts/app.js
+++ javascripts/app.js
@@ -2361,10 +2361,12 @@ App.HeaderView = Em.View.extend({
   templateName: 'header',
 
   responsiveNav: function() {
-    // Delay init of responsive nav so categories load and height
-    // is calculated correctly
+    var navSelector = '.nav_list';
+    // Delay init of responsive nav and hide original nav with css
+    // so categories load and height is calculated correctly.
+    // Then, show the nav in init() method
     Em.run.later(this, function() {
-      var nav = responsiveNav('.nav_list', { // Selector
+      var nav = responsiveNav(navSelector, { // Selector
         animate: true, // Boolean: Use CSS3 transitions, true or false
         transition: 284, // Integer: Speed of the transition, in milliseconds
         label: 'Nav', // String: Label for the navigation toggle
@@ -2374,6 +2376,9 @@ App.HeaderView = Em.View.extend({
         openPos: "relative", // String: Position of the opened nav, relative or static
         navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
         navActiveClass: "js-nav-active", // String: Class that is added to <html> element when nav is active
+        init: function() { // Function: Init callback
+          $(navSelector).css('display', 'block');
+        },
       });
     }, 500);
   }.on('didInsertElement'),
