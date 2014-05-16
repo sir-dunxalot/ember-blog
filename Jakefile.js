d198d5542a00728d208a7c087ca859c8966a8177
Duncan Graham Walker <walkerdu@bc.edu>
2014-05-15 22:54:57 -0400

    Build for deployment on May 15th 2014, 10:54:42 pm

--- Jakefile.js
+++ Jakefile.js
@@ -1,16 +1,23 @@
+var moment = require('moment');
+
 desc('This is the default task.');
 task('default', function (params) {
   console.log('This is the default task.');
 });
 
-desc('Publish public dir to github repo');
+desc('Publish the blog to your github repo\'s master branch');
 task('deploy', function (params) {
+  var commitMessage = '"Build for deployment on ' + new moment().format('MMMM Do YYYY, h:mm:ss a') + '"';
+
   var cmds = [
-    'git status',
-  ]
+    'brunch build --production',
+    'git add .',
+    'git commit -m ' + commitMessage,
+    'git push origin master'
+  ];
 
   jake.exec(cmds, {printStdout: true}, function () {
-    console.log('Ran git');
+    console.log('Successfully published the blog to origin/master');
     complete();
   });
 });
