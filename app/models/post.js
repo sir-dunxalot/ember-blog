e7b030f657f212641045c2fe9917059a6f8f5ccb
Duncan Graham Walker <walkerdu@bc.edu>
2014-11-04 19:56:33 -0500

    Working compiled fixtures, wahoo

--- app/models/post.js
+++ app/models/post.js
@@ -1,11 +1,30 @@
+import Em from 'ember';
 import Post from 'octosmashed/models/post';
 
-import test1 from '../posts/leak-attack/1';
-import test2 from '../posts/leak-attack/2';
-import test3 from '../posts/leak-attack/3';
+var posts = Em.A();
+
+// window.System = window.System || {};
+// window.System['import'] = function(moduleName) {
+//   return Em.RSVP.Promise.resolve(window.require(moduleName));
+// }
+
+/**
+Find all available modules and grab the posts
+*/
+
+for (var module in window.require.entries) {
+  var modulePath = module.toString();
+  var indexOfPosts = modulePath.indexOf('posts/');
+
+  if (indexOfPosts > -1) {
+    // System.import(modulePath).then(function(module) {
+      posts.pushObject(window.require(module)['default']);
+    // });
+  }
+}
 
 Post.reopenClass({
-  FIXTURES: [test1, test2, test3]
+  FIXTURES: posts
 });
 
 export default Post;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,3 @@
 import IndexRoute from 'octosmashed/routes/index';
 
 export default IndexRoute;
-
-// import Em from 'ember';
-
-// export default Em.Route.extend({
-//   model: function() {
-//     // console.log(this.store.all('post'));
-//     this.store.find('post');
-//   }
-// });
